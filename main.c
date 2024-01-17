90dfaeb31ec8ed2cb5d83f2bf62c6df4d35592cd
Rachid <djouaherrachid@gmail.com>
2024-01-17 16:44:43 +0100

    add connexion and menu interface

--- main.c
+++ main.c
@@ -3,85 +3,107 @@
 #include "sqlite/sqlite3.c"
 #include <gtk/gtk.h>
 
+static void activate(GtkApplication *app, gpointer user_data);
+static void on_tab_selected(GtkNotebook *notebook, GtkWidget *page, guint page_num, gpointer user_data);
+GtkWidget *create_menu_window(GtkApplication *app);
+GtkWidget *create_login_window(GtkApplication *app);
 
-static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
-{
-  g_print ("Hello World\n");
+//Animation lorsque l'utilisateur change d'onglet dans le menu
+static void on_tab_selected(GtkNotebook *notebook, GtkWidget *page, guint page_num, gpointer user_data) {
+    gtk_stack_set_visible_child_name(GTK_STACK(user_data), gtk_notebook_get_tab_label_text(notebook, page));
 }
 
-static void
-activate (GtkApplication *app,
-          gpointer        user_data)
-{
-  GtkWidget *window;
-  GtkWidget *button;
+//Menu principal
+GtkWidget *create_menu_window(GtkApplication *app) {
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Menu principal");
+    gtk_window_set_default_size(GTK_WINDOW(window), 1280, 720);
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Hello");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_container_add(GTK_CONTAINER(window), box);
 
-  button = gtk_button_new_with_label ("Hello World");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  gtk_window_set_child (GTK_WINDOW (window), button);
+    GtkWidget *stack = gtk_stack_new();
+    gtk_stack_set_transition_type(GTK_STACK(stack), GTK_STACK_TRANSITION_TYPE_SLIDE_LEFT_RIGHT);
 
-  gtk_window_present (GTK_WINDOW (window));
+    GtkWidget *notebook = gtk_notebook_new();
+    gtk_notebook_set_tab_pos(GTK_NOTEBOOK(notebook), GTK_POS_LEFT);
+    g_signal_connect(notebook, "switch-page", G_CALLBACK(on_tab_selected), stack);
+
+    const char *tab_labels[] = {"Onglet 1", "Onglet 2", "Onglet 3"};
+
+    for (int i = 0; i < G_N_ELEMENTS(tab_labels); i++) {
+        GtkWidget *page = gtk_label_new(g_strdup_printf("Contenu de l'onglet %d", i + 1));
+        GtkWidget *label = gtk_label_new(tab_labels[i]);
+        gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page, label);
+    }
+
+    GtkWidget *boxLeft = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_box_pack_start(GTK_BOX(boxLeft), notebook, TRUE, TRUE, 0);
+
+    GtkWidget *label1 = gtk_label_new("Contenu de l'onglet 1");
+    GtkWidget *label2 = gtk_label_new("Contenu de l'onglet 2");
+    GtkWidget *label3 = gtk_label_new("Contenu de l'onglet 3");
+
+    gtk_stack_add_named(GTK_STACK(stack), label1, "Onglet 1");
+    gtk_stack_add_named(GTK_STACK(stack), label2, "Onglet 2");
+    gtk_stack_add_named(GTK_STACK(stack), label3, "Onglet 3");
+
+    gtk_container_add(GTK_CONTAINER(box), boxLeft);
+    gtk_container_add(GTK_CONTAINER(box), stack);
+
+    return window;
 }
 
+//Fenêtre de connexion
+GtkWidget *create_login_window(GtkApplication *app) {
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Interface de Connexion");
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-int main(int    argc,
-      char **argv) {
-    // Declaration of a SQLite database pointer
-    sqlite3 *db;
-    // Pointer to store SQLite error messages
-    char *err_msg = 0;
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
 
-    // Attempt to open the "Website_Project.db" database
-    int rc = sqlite3_open("Website_Project.db", &db);
+    GtkWidget *username_label = gtk_label_new("Username:");
+    GtkWidget *username_entry = gtk_entry_new();
 
-    // Check if the database opening was successful
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Unable to open the database: %s\n", sqlite3_errmsg(db));
-        return rc;
-    }
+    GtkWidget *password_label = gtk_label_new("Password:");
+    GtkWidget *password_entry = gtk_entry_new();
+    gtk_entry_set_visibility(GTK_ENTRY(password_entry), FALSE);
 
-    // SQL query to create the "User" table if it doesn't exist
-    char *sql = "CREATE TABLE IF NOT EXISTS User (\n"
-                "                                         id INTEGER PRIMARY KEY,\n"
-                "                                         username TEXT NOT NULL,\n"
-                "                                         password TEXT NOT NULL\n"
-                ");";
+    GtkWidget *connect_button = gtk_button_new_with_label("Connexion");
+    g_signal_connect(connect_button, "clicked", G_CALLBACK(activate), app);
 
-    // Execute the SQL query
-    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    gtk_box_pack_start(GTK_BOX(vbox), username_label, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), username_entry, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), password_label, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), password_entry, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), connect_button, FALSE, FALSE, 0);
 
-    // Check if the execution of the SQL query was successful
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL Error: %s\n", err_msg);
+    return window;
+}
 
-        // Free the memory allocated for the error message
-        sqlite3_free(err_msg);
+//Procédure censée se lancer lorsque l'utilisateur appuie sur connexion et ouvrant le menu
+static void activate(GtkApplication *app, gpointer user_data) {
+    GtkWidget *menu_window = create_menu_window(app);
 
-        // Close the database
-        sqlite3_close(db);
+    // Afficher tous les widgets de la fenêtre principale
+    gtk_widget_show_all(menu_window);
 
-        return rc;
-    }
+}
 
-    // Close the database after a successful operation
-    sqlite3_close(db);
+int main(int argc, char *argv[]) {
+       // Initialiser GTK
+    gtk_init(&argc, &argv);
+    GtkApplication *app = gtk_application_new("com.example.Gtk4Tabs", G_APPLICATION_DEFAULT_FLAGS);
+    // Créer la fenêtre de connexion en appelant la fonction
+    GtkWidget *login_window = create_menu_window(app);
 
-      GtkApplication *app;
-     int status;
+    // Afficher tous les widgets
+    gtk_widget_show_all(login_window);
 
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
+    // Lancer la boucle principale GTK
+    gtk_main();
 
-  return status;
-    
-    // Indication of the success of the program
     return 0;
 }
