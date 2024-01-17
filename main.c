aaa70aec0be1d670ab6813e03c3226f6ebc61d0c
Rachid <djouaherrachid@gmail.com>
2024-01-17 01:10:43 +0100

    Initial commit

--- main.c
+++ main.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include "sqlite/sqlite3.h"
 #include "sqlite/sqlite3.c"
+#include <gtk/gtk.h>
 
-int main() {
-    //printf("SQLite version: %s\n", sqlite3_version);
-    //return 0;
 
+static void
+print_hello (GtkWidget *widget,
+             gpointer   data)
+{
+  g_print ("Hello World\n");
+}
+
+static void
+activate (GtkApplication *app,
+          gpointer        user_data)
+{
+  GtkWidget *window;
+  GtkWidget *button;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Hello");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+  button = gtk_button_new_with_label ("Hello World");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  gtk_window_set_child (GTK_WINDOW (window), button);
+
+  gtk_window_present (GTK_WINDOW (window));
+}
+
+
+int main(int    argc,
+      char **argv) {
     // Declaration of a SQLite database pointer
     sqlite3 *db;
     // Pointer to store SQLite error messages
@@ -46,6 +72,16 @@ int main() {
     // Close the database after a successful operation
     sqlite3_close(db);
 
+      GtkApplication *app;
+     int status;
+
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+
+  return status;
+    
     // Indication of the success of the program
     return 0;
 }
